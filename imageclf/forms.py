f72078aa2484b4c5deea494ef737dd63813832ec
kyutatsu <qtatsunishiura@gmial.com>
2019-06-19 16:24:38 +0900

    templateにbootstrap適応, viewsで表示画像数指定機能を追加。

--- imageclf/forms.py
+++ imageclf/forms.py
@@ -2,12 +2,21 @@ from django import forms
 
 
 class IllustratorForm(forms.Form):
-    name = forms.CharField(max_length=100)
+    name = forms.CharField(max_length=100, label="ユーザ ID:")
     CHOICES = [
-            ('clf', '分類する'),
+            ('clf', 'イラスト/写真を分類する'),
             ('noclf', '分類しない'),
     ]
     clf_status = forms.ChoiceField(
             widget=forms.RadioSelect,
             choices = CHOICES,
+            label=""
+    )
+    img_number = forms.IntegerField(
+            initial=50,
+            label='取得枚数:',
+            help_text='取得枚数が多すぎる場合、取得と分類に1,2分かかることがあります。\
+                      <br>また画像以外のツイートが非常に多いユーザの場合、\
+                      ツイート取得に時間がかかります。<br>\
+                      読み込まれない場合、取得枚数を減らしてみてください。'
     )
--- imageclf/views.py
+++ imageclf/views.py
@@ -33,20 +33,20 @@ def index(request):
     )
 
 def get_and_classify_images(request):
-    """twitter user名(@以下)から,可能な限り画像ツイートを回収する。
+    """twitter user名(@以下)から,画像ツイートを取得する。
 
-    回収した画像を分類して表示する。
+    取得した画像を分類して表示する。
     """
     if request.method == 'POST':
         form = IllustratorForm(request.POST)
         if form.is_valid():
+            max_num_img = form.cleaned_data.get('img_number')
+            # tweets_with_photo = []  # tweet単位
+            tweets_for_render = []  # 画像単位
             twitter_session = OAuth1Session(APIKEY, APISECRETKEY)
             # 初回はmax_idを指定したくない。Noneにすれば指定なしと同じになる。
             max_id = None
-            # 一旦変更(後で最大まで直す)
-            # n = 3200 / 200   # ツイート取得可能数
-            n = 5
-            tweets_with_photo = []
+            n = 3200 / 200   # ツイート取得可能数
             for i in range(int(n)):
                 params = {
                         'screen_name': form.cleaned_data.get('name'),
@@ -62,36 +62,26 @@ def get_and_classify_images(request):
                 if response.status_code != 200:
                     return HttpResponse('取得失敗しました')
                 tweets = json.loads(response.text)
-                # 3,200ツイート以下だった場合、取得できなかった時点で抜ける。
+                # 3,200ツイート以下だった場合,取得できなかった時点で抜ける。
                 if tweets == []:
                     break
                 for tweetobj in tweets:
-                    # twitterは複数種類mediaを同時に添付できないのでidx0のみ見れば良いはず.
+                    # id=0のmediatypeをチェックする.
                     try:
                         mediatype = tweetobj['extended_entities']['media'][0]['type'] 
                     except Exception:
                         mediatype = None
-                    if mediatype and mediatype == 'photo':
+                    if mediatype and (mediatype == 'photo'):
                         # mediasはgenerator.
                         medias = get_items_from_tweet(tweetobj)
-                        tweets_with_photo.append(medias)
-                # 最後(oldest)のtw objectのid由来。
+                        # 画像ごとのobjectにしてlistに入れる。
+                        tweets_for_render.extend(list(medias))
+                if len(tweets_for_render) >= max_num_img:
+                    break
                 max_id = tweetobj.get('id') - 1
-            if tweets_with_photo == []:
+            if tweets_for_render == []:
                 return HttpResponse('画像はありません')
 
-            # dbへの保存処理ブロック>>わかりにくいから関数化する？
-            tweets_for_render = [] # templatでgeneratorが動かないからこうした。書き直したい.
-            for medias in tweets_with_photo:
-                for media in medias:
-                    # 表示用にtextをbynary=>strにする
-                    media_cp = media.copy()
-                    media_cp['text'] = codecs.decode(media_cp.get('text',''))
-                    # rendering用の画像obj(自作の辞書)のリスト
-                    tweets_for_render.append(media_cp)
-                    # Image, PILのImageと同名だからこの名前よくない。
-                    # image = Image(**media)
-           
             if form.cleaned_data.get('clf_status') == 'clf':
                 # すでにmediaが存在しないかチェック。
                 # process with classifier
@@ -122,7 +112,6 @@ def get_and_classify_images(request):
                         )
                         qs = qs.reshape((1, -1))
                         features = np.concatenate((mean, variance, qs), axis=1)
-                        # features = features.reshape((1,-1))
                         features_scaled = scaler.transform(features)
                         predict = clf.predict(features_scaled)[0]
                         if predict == 0:
@@ -133,7 +122,7 @@ def get_and_classify_images(request):
                             proba = clf.predict_proba(features_scaled)[0,1]
                             media['proba'] = proba
                             photos.append(media)
-            else: # 分類しない時
+            else:  # 分類しない時
                 return render(
                     request,
                     'imageclf/show_images.html', 
@@ -142,43 +131,6 @@ def get_and_classify_images(request):
                     }
                 )
 
-                        # np.arrayの、ndarray[ndarray]でboolianでインデックス指定使いたいので
-                       # data_array = ""
-                       # for_render_data = ""
-                       # for num, media in enumerate(tweets_for_render):
-                       #     temp_params = np.array(
-                       #         [
-                       #             int(media['retweet_count']), 
-                       #             int(media['favorite_count']),
-                       #             int(media['has_multiple_media']),
-                       #         ]
-                       #     )
-                       #     temp_render_params = np.array(
-                       #         [
-                       #             media['media_id_str'],
-                       #             media['media_url_https'],
-                       #             media['text'],
-                       #             media['tweet_url'],
-                       #         ]
-                       #     )
-                       #     if num == 0:
-                       #         data_array = temp_params.reshape((1,-1))
-                       #         for_render_data = temp_render_params.reshape((1,-1))
-                       #     else:
-                       #         data_array = np.concatenate(
-                       #             (data_array, temp_params.reshape((1, -1)))
-                       #         )
-                       #         for_render_data = np.concatenate(
-                       #             (for_render_data, temp_render_params.reshape((1,-1)))
-                       #         )
-                       # # ここで前のデータ使ってfitしないとダメなの欠陥では？
-                       # data_array_norm = scaler.transform(data_array)
-                       # classified = knn.predict(data_array_norm)
-
-                        # illust_data = for_render_data[classified=='illust']
-                        # photo_data = for_render_data[classified=='photo']
-
-                       #  screen_data = for_render_data[classified=='screen']
             # redirectした方がいい
             illusts = sorted(illusts, key=lambda x: x['proba'], reverse=True)
             photos = sorted(photos, key= lambda x: x['proba'], reverse=True)
@@ -188,7 +140,6 @@ def get_and_classify_images(request):
                 {
                     'illust_data': illusts,
                     'photo_data': photos,
-                   #  'screen_data': list(screen_data),
                 }
             )
     else:
@@ -198,6 +149,6 @@ def get_and_classify_images(request):
             request,
             'imageclf/get_illustrator_name.html',
             {
-                'form': form,
+                'forms': form,
             }
     )
